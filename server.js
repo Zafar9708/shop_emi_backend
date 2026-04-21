c1228919b8d8628192429d0bc727c81187b1fb64
Zafar9708 <zafarekhlaque@gmail.com>
2026-04-22 01:06:19 +0530

    added test url

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/api/test', (req, res) => {
+  res.send("API working ✅");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
